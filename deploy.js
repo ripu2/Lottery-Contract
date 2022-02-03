f3dfd356d45d4fe59bfb6d5781f5e380ca8017f8
Ripudaman <44752831+ripu2@users.noreply.github.com>
2022-02-03 22:12:31 +0530

    few truffle related fixes and deployment script fix

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 const Web3 = require('web3')
 
-const { interface, bytecode } = require('./contracts')
+const { interface, bytecode } = require('./compile')
 
 const provider = new HDWalletProvider(
     'zero idle knock obtain science cotton equal soon maid shy couple ability',
@@ -13,18 +13,22 @@ const web3 = new Web3(provider)
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
 
-    console.log('Attempting to deploy form =====>', accounts[0], '🚀')
-
     if(accounts) {
-        const res = await new Web3.eth.Contract(JSON.parse(interface))
+        console.log('Attempting to deploy form =====>', accounts[0], '🚀')
+
+        const res = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode })
         .send({ gas: '1000000', from: accounts[0] })
 
         if(res) {
-            console.log('Contract deployed at ===>', response.options.address, '🔥');
+            console.log('interface ====>', interface)
+            console.log('Contract deployed at ===>', res.options.address, '🔥');
             provider.engine.stop()
         }
     } else {
         console.log('failed to deploy contract ☹️')
     }
 }
+
+deploy()
+
